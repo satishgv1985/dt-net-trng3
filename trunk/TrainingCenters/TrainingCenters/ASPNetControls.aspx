7ee843e91ebaa25f4e1365a9642c0b189a306fd1
satishgv1985 <satishgv1985@42752b17-02da-1fa7-dae1-3d8c96cd1f48>
2012-04-04 04:01:18 +0000

    adding files for the first time

---
+++ trunk/TrainingCenters/TrainingCenters/ASPNetControls.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ASPNetControls.aspx.cs"
+    Inherits="TrainingCenters.ASPNetControls" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Courses:
+        <asp:DropDownList ID="ddlCourses" runat="server" AutoPostBack="true" 
+            onselectedindexchanged="ddlCourses_SelectedIndexChanged">
+            <asp:ListItem Text="--Select Course--" Value="0">
+            </asp:ListItem>
+            <asp:ListItem Text="Intermediate" Value="1"></asp:ListItem>
+            <asp:ListItem Text="B.Tech" Value="2"></asp:ListItem>
+        </asp:DropDownList>
+        <asp:RequiredFieldValidator ID="rfvCourses" Display="Dynamic" runat="server" ForeColor="Red" ErrorMessage="* Please Select Course" ControlToValidate="ddlCourses" InitialValue="0"></asp:RequiredFieldValidator>
+        <br />
+        Subjects:
+        <asp:DropDownList ID="ddlSubjects" runat="server">
+        <asp:ListItem Text="--Select Course First--" Value="0"></asp:ListItem>
+        </asp:DropDownList>
+
+        <br />
+
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" />
+    </div> 
+    </form>
+</body>
+</html>
---
+++ trunk/TrainingCenters/TrainingCenters/WebForm1.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TrainingCenters
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+            }
+        }
+
+
+
+        protected void ddlState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlCity.Items.Clear();
+
+            if (ddlState.SelectedValue == "0")
+            {
+                ListItem licity = new ListItem("--Select State First--", "0");
+                ddlCity.Items.Add(licity);
+            }
+            else if (ddlState.SelectedValue == "1")
+            {
+                ListItem licity = new ListItem("Hyderabad", "1");
+                ddlCity.Items.Add(licity);
+                licity = new ListItem("Secundrerabad", "2");
+                ddlCity.Items.Add(licity);
+
+                licity = new ListItem("Vizag", "3");
+                ddlCity.Items.Add(licity);
+                licity = new ListItem("Vijayawada", "4");
+                ddlCity.Items.Add(licity);
+            }
+            else if (ddlState.SelectedValue == "2")
+            {
+                ListItem licity = new ListItem("Bangalore", "1");
+                ddlCity.Items.Add(licity);
+                licity = new ListItem("Mysore", "2");
+                ddlCity.Items.Add(licity);
+
+                licity = new ListItem("Mangalore", "3");
+                ddlCity.Items.Add(licity);
+            }
+        }
+
+
+
+
+
+
+    }
+}
