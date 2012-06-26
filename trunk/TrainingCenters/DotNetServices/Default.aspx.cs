e2502d92f5bfccbf5fa2a78474e634dd77bae070
shilpayennam@gmail.com <shilpayennam@gmail.com@42752b17-02da-1fa7-dae1-3d8c96cd1f48>
2012-06-26 06:14:34 +0000

    add web services files

---
+++ trunk/TrainingCenters/DotNetServices/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TCProject;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Institutes inst = new Institutes();
+        string st = inst.HelloWorld();
+        lblResp.Text = st;
+
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        Calculator.DotNetCalculator dnc = new Calculator.DotNetCalculator();
+        int sum = dnc.Add(Convert.ToInt32(txtAdd.Text), Convert.ToInt32(txtAdd2.Text));
+        lblReponse.Text = sum.ToString();
+    }
+}
