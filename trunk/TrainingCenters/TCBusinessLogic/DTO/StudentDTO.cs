68485c44b6eab9a1fcc6743f965b8f86ae8fbfd6
shivap001@gmail.com <shivap001@gmail.com@42752b17-02da-1fa7-dae1-3d8c96cd1f48>
2012-07-01 19:18:48 +0000

    added StudentDTO.cs and StudentDal.cs added GetStusentByUserName.asmx

---
+++ trunk/TrainingCenters/TCBusinessLogic/DTO/StudentDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCBusinessLogic.DTO
+{
+    public class StudentDTO
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string Qualification { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        public string Area { get; set; }
+        public string MobileNo { get; set; }
+        public string  EmailID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int CityID { get; set; }
+        public int StateID { get; set; }
+
+        
+    }
+}
---
+++ trunk/TrainingCenters/TrainingCenters/training/shiva/GetStusentByUserName.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="GetStusentByUserName.asmx.cs" Class="TrainingCenters.training.shiva.GetStusentByUserName" %>
