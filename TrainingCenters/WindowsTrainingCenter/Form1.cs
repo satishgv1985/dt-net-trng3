ba809d33b8ad9e82f2d0c01ef26f74e624592c6a
shijith.ss <shijith.ss@42752b17-02da-1fa7-dae1-3d8c96cd1f48>
2012-07-02 05:39:41 +0000

    windows application

---
+++ TrainingCenters/WindowsTrainingCenter/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TCBusinessLogic.DTO;
+using TCBusinessLogic.DAL;
+
+namespace WindowsTrainingCenter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InstituteDTO ins = InstituteDAL.ValidateInstitute(textBox1.Text,textBox2.Text);
+            if (ins == null)
+            {
+
+                label3.Text = "Successful Login.";
+            }
+            else
+            {
+                label3.Text = "Login Failed!";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
